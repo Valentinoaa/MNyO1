71a2b28f0db0c1028abbc7bdfb2a71b27de86031
Alejo <128553535+kekontas@users.noreply.github.com>
2024-03-21 13:46:15 -0300

    Co-authored-by: Valentino Arbelaiz <Valentinoaa@users.noreply.github.com>

--- lagrange.py
+++ lagrange.py
@@ -12,7 +12,7 @@ def plot_a(interval: np.ndarray = np.linspace(-4, 4, 1000)):
         x = scipy.interpolate.lagrange(points_a, f_a(points_a))
         plt.plot(interval, x(interval), label=f'n = {n}')
         plt.plot(points_a, f_a(points_a), 'o', label='Puntos de interpolación')
-        plt.plot(np.linspace(-4, 4, 1000), x(np.linspace(-4, 4, 1000)), label='Polinomio interpolante')
+        plt.plot(np.linspace(-4, 4, 100), x(np.linspace(-4, 4, 100)), label='Polinomio interpolante')
     
     plt.plot(interval, f_a(interval))
     plt.title('Función $f_a(x)$')
@@ -32,32 +32,31 @@ def plot_b(x1: np.ndarray = np.linspace(-1, 1, 100), x2: np.ndarray = np.linspac
     ax.set_zlabel('$f_b(x_1, x_2)$')
     plt.show()
 
-def interpolatedB(x1: np.ndarray = np.linspace(-1, 1, 100), x2: np.ndarray = np.linspace(-1, 1, 100)):
-    points_x1 = divide_interval(x1, 5)
-    points_x2 = divide_interval(x2, 5)
 
-    x1_plot_b, x2_plot_b = np.meshgrid(points_x1, points_x2)
-    print("-")
-    f_b_inter = scipy.interpolate.griddata(points=(x1_plot_b, x2_plot_b), values=f_b(x1_plot_b, x2_plot_b), xi=(x1_plot_b, x2_plot_b), method='linear')
-    print(f_b_inter.shape)    
+
+def interpolated_b(x1: np.ndarray = np.linspace(-1, 1, 100), x2: np.ndarray = np.linspace(-1, 1, 100)):
+    grid_x, grid_y = np.meshgrid(x1, x2)
+    grid_z = scipy.interpolate.griddata((grid_x.flatten(), grid_y.flatten()), f_b(grid_x, grid_y).flatten(), (grid_x, grid_y), method='linear')
+    
     fig = plt.figure(figsize=(8, 6))
     ax = fig.add_subplot(111, projection='3d')
-    ax.plot_surface(x1, x2, f_b_inter(x1, x2))
-    ax.set_title('Función $f_b(x_1, x_2)$')
+    ax.plot_surface(grid_x, grid_y, grid_z)
+    ax.set_title('Función interpolada con 10000 puntos $f_b(x_1, x_2)$')
     ax.set_xlabel('$x_1$')
     ax.set_ylabel('$x_2$')
     ax.set_zlabel('$f_b(x_1, x_2)$')
-    plt.show()  
+    plt.show()
+
 
 def divide_interval(interval: np.ndarray, n: int) -> np.ndarray:
     points = np.linspace(interval[0], interval[-1], num=n)
     return points
 
 def main():
-    #plot_a()
-    #plot_b()
+    plot_a()
+    plot_b()
     
-    interpolatedB()
+    interpolated_b()
     
 if __name__ == "__main__":
     main()
