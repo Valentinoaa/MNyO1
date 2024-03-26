c6ea0138571d4867209f9d53494bd3e5f342ad83
Alejo <128553535+kekontas@users.noreply.github.com>
2024-03-26 16:56:33 -0300

    chebyshevs

--- puntoa.py
+++ puntoa.py
@@ -67,6 +67,66 @@ def interpolated_b(x1: np.ndarray = np.linspace(-1, 1, 100), x2: np.ndarray = np
     plt.show()
 
 
+def chebyshev_roots(x_left, x_right, N):
+    radius = (x_right - x_left) / 2.
+    center = (x_right + x_left) / 2.    
+    return center + radius * np.cos(np.pi - np.arange(N+1)*np.pi/N)
+
+def interpolateAchevyshev():
+    x = chebyshev_roots(-4, 4, 20)
+
+    f_cheb = f_a(x) 
+    f_inter = scipy.interpolate.lagrange(x, f_cheb)
+
+    xx = np.linspace(-4, 4, 300)
+    plt.figure(figsize=(8, 6))
+    plt.plot(x, f_cheb, label='Chebyshev nodes', marker='o', color='blue', linestyle='None')
+    plt.plot(xx, f_inter(xx), label='Interpolated function', color='blue')  
+    plt.plot(xx, f_a(xx), label='Original function', color='black')
+    plt.title('Interpolated function with Chebyshev nodes')
+    plt.xlabel('x')
+    plt.ylabel('$f_a(x)$')
+    plt.legend()
+    plt.show()
+
+def interpolateBchevychev(x1: np.ndarray = np.linspace(-1, 1, 100), x2: np.ndarray = np.linspace(-1, 1, 100)):
+    x1_plot_b, x2_plot_b = np.meshgrid(x1, x2)
+
+    x1 = chebyshev_roots(-1, 1, 20)
+    x2 = chebyshev_roots(-1, 1, 20)
+
+    # Create meshgrid
+    x1, x2 = np.meshgrid(x1, x2)
+
+    # Generate interpolated values
+    grid_z = scipy.interpolate.griddata((x1.flatten(), x2.flatten()), f_b(x1, x2).flatten(), (x1, x2), method='linear')
+
+    # Plotting
+    fig = plt.figure(figsize=(12, 6))
+
+    # Original function subplot
+    ax = fig.add_subplot(121, projection='3d')
+    ax.plot_surface(x1_plot_b, x2_plot_b, f_b(x1_plot_b, x2_plot_b), edgecolor='none', alpha=0.8, antialiased=True)
+    ax.set_title('Original function $f_b(x_1, x_2)$')
+    ax.set_xlabel('$x_1$')
+    ax.set_ylabel('$x_2$')
+    ax.set_zlabel('$f_b(x_1, x_2)$')
+
+    # Interpolated function subplot
+    ay = fig.add_subplot(122, projection='3d')
+    ay.plot_surface(x1, x2, grid_z, cmap='viridis', edgecolor='none', alpha=0.8, antialiased=True)
+    ay.set_title('Interpolated function with Chebyshev nodes $f_b(x_1, x_2)$')
+    ay.set_xlabel('$x_1$')
+    ay.set_ylabel('$x_2$')
+    ay.set_zlabel('$f_b(x_1, x_2)$')
+
+    plt.tight_layout()
+    plt.show()
+    
+    plt.show()
+
+
+
 def divide_interval(interval: np.ndarray, n: int) -> np.ndarray:
     points = np.linspace(interval[0], interval[-1], num=n)
     return points
@@ -75,9 +135,11 @@ def calculate_error(f, g, interval):
     return scipy.integrate.quad(lambda x: abs((f(x) - g(x))), interval[0], interval[-1])
 
 def main():
-    # plot_a()
+    #plot_a()
     plot_b()
-    interpolated_b()
+    #interpolated_b()
+    interpolateAchevyshev()
+    interpolateBchevychev()
 
 
     
