35d2981d231b7587957033f5ea0caa3e277d32d4
Valentino Arbelaiz <valentinoarbelaiz@gmail.com>
2024-03-28 16:57:01 -0300

    JaAAaa

--- punto2a.py
+++ punto2a.py
@@ -10,9 +10,7 @@ def main():
     plt.xlabel('Coordenada X')
     plt.ylabel('Coordenada Y')
 
-
     # plt.plot(inter['c1'], inter['c2'], 'b')    
-
 
     plot_interpolation(inter['c1'], inter['c2'], linestyle='dashed')
 
@@ -23,6 +21,7 @@ def main():
     plt.show()
 
 def open_csv(file: str = 'mnyo_meduciones', column1: str = 'c1', column2: str = 'c2' ) -> pd.DataFrame:
+
     inter = pd.read_csv(f'Mediciones/{file}.csv' , header=None, sep=' ')
 
     inter.columns = [column1, column2]
@@ -31,6 +30,8 @@ def open_csv(file: str = 'mnyo_meduciones', column1: str = 'c1', column2: str = 
 
 def plot_interpolation(y1: np.ndarray = np.linspace(-1, 1, 100), y2: np.ndarray = np.linspace(-1, 1, 100), points=10, color='r', kind='cubic', linestyle='solid'):
     x = np.linspace(0, points, points)
+    # F(x) -> y1, y2
+    # [0, 1, 2 .. 10]
     grid = scipy.interpolate.interp1d(x, [y1, y2], kind=kind)
     plt.plot(grid(np.linspace(0, points, 100))[0], grid(np.linspace(0, points, 100))[1], color, linestyle=linestyle)
 
--- punto2b.py
+++ punto2b.py
@@ -10,14 +10,35 @@ def main():
     plt.figure(figsize=(8, 6))
 
     fst = open_csv('mnyo_mediciones')
-    
+
     plot_interpolation(fst["c1"], fst["c2"])
 
     scd = open_csv('mnyo_mediciones2')
 
     plot_interpolation(scd["c1"], scd["c2"], 4, 'b')
 
+
+    f = intersection(fst["c2"], scd["c2"])
+
+    print(f(3))
     plt.show()
+
+
+def intersection(fst, scd):
+    dom_fst = np.linspace(0, 10, len(fst))
+    dom_scd = np.linspace(0, 10, len(scd))
+
+    f = scipy.interpolate.CubicSpline(dom_fst, fst)
+    g = scipy.interpolate.CubicSpline(dom_scd, scd)
+
+    return lambda x: f(x) - g(x)        
+    
+
+def interpolate(y, dom):
+
+    f = scipy.interpolate.CubicSpline(dom, y)
+
+    return f(dom)
 
 
 if __name__ == "__main__":
